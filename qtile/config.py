7f5401b1d14b2f3193470e86d86837919209fa43
Juanki0396 <juanki0396@gmail.com>
2022-07-18 19:33:15 +0200

    Changed layouts

--- qtile/config.py
+++ qtile/config.py
@@ -35,6 +35,8 @@ subprocess.run(["/home/quito/bin/autostart.sh"]) #Calls the initialization scrip
 mod = "mod4"
 terminal = guess_terminal()
 
+# ---------------------------- KEYS -----------------------------------
+
 keys = [
     #Spawn programs
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
@@ -81,6 +83,8 @@ keys = [
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts")
 ]
 
+# --------------------------- GROUPS ------------------------------------
+
 groups = [
     Group(name="INT", spawn="firefox", init=True),
     Group(name="TERM", spawn=terminal, init=True),
@@ -113,21 +117,33 @@ for i, group in enumerate(groups):
         ]
     )
 
-layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-    layout.Max(),
+# ----------------- LAYOUTS---------------------------
+
+layouts_config = {
+    "border_focus": "#164DA3".lower(),
+    "border_width": 2,
+    "margin": 4
+}
+
+selected_layouts = [
+    layout.Columns,
+    layout.Max,
     # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    # layout.Stack,
+    # layout.Bsp,
+    # layout.Matrix,
+    layout.MonadTall,
+    # layout.MonadWide,
+    # layout.RatioTile,
+    # layout.Tile,
+    # layout.TreeTab,
+    # layout.VerticalTile,
+    # layout.Zoomy,
 ]
+
+layouts = [lout(**layouts_config) for lout in selected_layouts]
+
+# ----------------- SCREENS --------------------------
 
 widget_defaults = dict(
     font="sans",
@@ -138,7 +154,7 @@ extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        bottom=bar.Bar(
+        top=bar.Bar(
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
@@ -163,12 +179,16 @@ screens = [
     ),
 ]
 
+# ------------------------------------ MOUSE ----------------------------------
+
 # Drag floating layouts.
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
+
+# ------------------------ CONSTANTS -------------------------
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
@@ -187,6 +207,7 @@ floating_layout = layout.Floating(
         Match(title="pinentry"),  # GPG key password entry
     ]
 )
+
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
